cc47bf930796bb13fabf428fe30495fe28696aac
diego-vidal <vidaldiego@gmail.com>
2015-07-04 18:10:11 -0400

    Removed WebApi refrences

--- Spellfire.Web/Controllers/BaseController.cs
+++ Spellfire.Web/Controllers/BaseController.cs
@@ -1,10 +1,7 @@
-﻿using Spellfire.Web.Attributes;
-using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Spellfire.Web.Controllers
 {
-    [ElmahHandleError]
     public class BaseController : Controller
     {
     }
